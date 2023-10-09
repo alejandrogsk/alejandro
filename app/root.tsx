e7b1b4818e8f682df5fc878e91097443b0ea3b52
alejandrogsk <alejandrogsk9900@gmail.com>
2023-10-09 10:25:29 -0300

    Google Analytics

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import { json, type LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,21 +7,37 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+  useLocation,
 } from "@remix-run/react";
 import stylesheet from "~/tailwind.css";
 import Navigation from "./components/Layout/Navigation";
 import Grid from "./components/Layout/Grid";
-
-
+import { useEffect } from "react";
+import * as gtag from "~/utils/gtags.client";
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
   { rel: "stylesheet", href: stylesheet }
 ];
 
+// Load the GA tracking id from the .env
+export const loader = async () => {
+  return json({ gaTrackingId: process.env.GA_TRACKING_ID });
+};
 
 
 
 export default function App() {
+
+  const location = useLocation();
+  const { gaTrackingId } = useLoaderData<typeof loader>();
+
+  useEffect(() => {
+    if (gaTrackingId?.length) {
+      gtag.pageview(location.pathname, gaTrackingId);
+    }
+  }, [location, gaTrackingId]);
+
   return (
     <html lang="en">
       <head>
@@ -37,6 +53,7 @@ export default function App() {
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
+
       </body>
     </html>
   );
